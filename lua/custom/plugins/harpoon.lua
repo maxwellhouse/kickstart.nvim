b99b2df958086f397a616e0227299501053f345a
Jeff Maxwell <maxwellhouse@gmail.com>
2023-12-04 09:38:44 -0600

    Moving harpoon to harpoon2

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,20 +1,20 @@
 
 return {
     'ThePrimeagen/harpoon',
+    branch = "harpoon2",
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
     config = function ()
-        require('harpoon').setup {}
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+        local harpoon = require("harpoon")
+        harpoon:setup()
 
-        vim.keymap.set("n", "<leader>m", mark.add_file, { desc = '[M]ark file for Harpoon' } )
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = 'Toggle Harpoon quick m[e]nu' } )
-        vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = 'Select Harpoon file 1' } )
-        vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end, { desc = 'Select Harpoon file 2' } )
-        vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end, { desc = 'Select Harpoon file 3' } )
-        vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end, { desc = 'Select Harpoon file 4' } )
+        vim.keymap.set("n", "<leader>m", function() harpoon.list():append() end, { desc = '[M]ark file for Harpoon' } )
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Toggle Harpoon quick m[e]nu' } )
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = 'Select Harpoon file 1' } )
+        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end, { desc = 'Select Harpoon file 2' } )
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end, { desc = 'Select Harpoon file 3' } )
+        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end, { desc = 'Select Harpoon file 4' } )
     end,
 }
 
