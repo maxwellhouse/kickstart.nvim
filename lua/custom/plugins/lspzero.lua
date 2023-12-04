5ab42799fbd982bb4b390dfe875754845ae8db3b
Jeff Maxwell <jeff.maxwell@oneasg.com>
2023-12-04 16:03:44 -0600

    Fixing some snippit stuff

--- lua/custom/plugins/lspzero.lua
+++ lua/custom/plugins/lspzero.lua
@@ -22,6 +22,15 @@ return {
     event = 'InsertEnter',
     dependencies = {
       {'L3MON4D3/LuaSnip'},
+      {'saadparwaiz1/cmp_luasnip'},
+
+      -- Adds LSP completion capabilities
+      {'hrsh7th/cmp-nvim-lsp'},
+
+      -- Adds a number of user-friendly snippets
+      {'rafamadriz/friendly-snippets'},
+      -- Additional lua configuration, makes nvim stuff amazing!
+      {'folke/neodev.nvim'},
     },
     config = function()
       -- Here is where you configure the autocompletion settings.
